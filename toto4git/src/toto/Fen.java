1446f68d23b8b966cd9502603a8c20432e80af34
gl <gl@localhost>
2020-01-03 11:39:43 +0100

    essai6.

--- toto4git/src/toto/Fen.java
+++ toto4git/src/toto/Fen.java
@@ -16,7 +16,7 @@ public class Fen extends JFrame implements ActionListener {
  
 	private JButton b = new JButton("toto");
 	private JPanel p = new JPanel();
-	private JTextField jtf=new JTextField("5");
+	private JTextField jtf=new JTextField("6");
 	 
 	public Fen() {
 		Container cc;
