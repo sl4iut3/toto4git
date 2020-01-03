bb79ddf39ac6a0d5b1723091173a478760575728
gl <gl@localhost>
2020-01-03 12:23:51 +0100

    essai12

--- toto4git/src/toto/Fen.java
+++ toto4git/src/toto/Fen.java
@@ -13,47 +13,47 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Fen extends JFrame implements ActionListener {
- 
+
 	private JButton b = new JButton("toto");
 	private JPanel p = new JPanel();
-	private JTextField jtf=new JTextField("10");
-	 
+	private JTextField jtf = new JTextField("10");
+	Container c;
+
 	public Fen() {
-		Container cc;
 		c = this.getContentPane();
-		c.add(b,BorderLayout.NORTH);
-		c.add(jtf,BorderLayout.CENTER);
+		c.add(b, BorderLayout.NORTH);
+		c.add(jtf, BorderLayout.CENTER);
 		c.add(p, BorderLayout.SOUTH);
 		p.add(new JButton());
-		//p.setPreferredSize(getMaximumSize());
+		// p.setPreferredSize(getMaximumSize());
 		b.addActionListener(this);
-		setBounds(10,10,200,200);
+		setBounds(10, 10, 200, 200);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setVisible(true);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated meth
-		Fen f=new Fen();
-		
+		Fen f = new Fen();
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		//Fen f=new Fen();
-		int nb=Integer.parseInt(jtf.getText());
-		System.out.println("nb="+nb);
-		JOptionPane.showMessageDialog(this, "create "+nb+" JButtons ?");
+		// Fen f=new Fen();
+		int nb = Integer.parseInt(jtf.getText());
+		System.out.println("nb=" + nb);
+		JOptionPane.showMessageDialog(this, "create " + nb + " JButtons ?");
 		c.remove(p);
-		p=new JPanel(new GridLayout(3, 3)) ;
-		for (int i=0;i<nb;i++) {
-			p.add(new JButton("B"+i));
+		p = new JPanel(new GridLayout(3, 3));
+		for (int i = 0; i < nb; i++) {
+			p.add(new JButton("B" + i));
 		}
-		c.add(p,BorderLayout.SOUTH);
+		c.add(p, BorderLayout.SOUTH);
 		revalidate();
-		//repaint();
-		//update(getGraphics());
+		// repaint();
+		// update(getGraphics());
 
 	}
 
